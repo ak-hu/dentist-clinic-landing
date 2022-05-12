f4765980e053b066ecf1be377d61b26b396553fa
ak-hu <68818657+ak-hu@users.noreply.github.com>
2022-05-12 12:50:01 +0200

    Add files via upload

---
+++ clinic/static/clinic/js/script.js
@@ -0,0 +1,19 @@
+let burger_menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header nav');
+let header = document.querySelector('.header');
+
+burger_menu.onclick = function click() {
+burger_menu.classList.toggle('fa-times');
+navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+	burger_menu.classList.remove('fa-times');
+	navbar.classList.remove('active');
+
+	if (window.scrollY > 0) {
+		header.classList.add('active');
+	}else{
+		header.classList.remove('active');
+	}
+}
