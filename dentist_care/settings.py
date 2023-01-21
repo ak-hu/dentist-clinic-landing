a3f2119e3b5182489864a9372578f9864aa79cdb
Artem <arty.lindemann@gmail.com>
2023-01-21 20:59:20 +0600

    Update settings.py

--- dentist_care/settings.py
+++ dentist_care/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)#6h%y@4)gt%5jn(i=it)2#q0-a9dh75t((h_=n+)xfizbvq%4'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
